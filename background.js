e4e54aec7925a5fd3adb5a1d45e02fa5dcf454cf
Caleb Planchet <calebplanchet@vitatech.co>
2020-12-03 12:07:30 -0500

    Basic POST communication and encoding

--- background.js
+++ background.js
@@ -3,14 +3,19 @@ chrome.runtime.onMessage.addListener(
 		if(request.message === "Test"){
 			console.log(request.url);
 			var f = document.createElement('form');
-			f.action = 'http://virustotal.com/gui/search/https%253A%252F%252Fwww.google.com'
+			var vtURL = 'http://virustotal.com/gui/search/';
+			var tempURL = 'https://www.google.com';
+			tempURL = encodeURIComponent(tempURL);
+			console.log(vtURL + tempURL)
+			f.action = vtURL + tempURL;
+			window.open(vtURL + tempURL);
 			f.method = 'put';
-			//var i = document.createElement('input');
-			//i.type = 'hidden';
-			//i.name = 'url'
-			//i.value = request.url;
-			//f.appendChild(i);
-			//document.body.appendChild(f);
+			var i = document.createElement('input');
+			i.type = 'hidden';
+			i.name = 'url'
+			i.value = request.url;
+			f.appendChild(i);
+			document.body.appendChild(f);
 			f.submit();
 		}
 	}
--- content.js
+++ content.js
@@ -4,3 +4,4 @@ for(var i = 0; i < links.length; i++){
 	console.log(links.item(i).href);
 }
 chrome.runtime.sendMessage({"message":"Test", "url":links.item(0).href});
+console.log("why");
